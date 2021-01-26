ac07d12ff8ff438f8740ec2ec3f2470bfda96daa
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-26 19:06:58 +0100

    Created simple todo class

--- todo/todo.js
+++ todo/todo.js
@@ -1,6 +1,27 @@
-export function Todo () {
-    this.content = ''
-    this.id = 1
-    this.completed = false
-}
+import { v4 as uuidv4 } from 'uuid';
 
+export class Todo {
+    todos = [
+        {
+            content: '',
+            id: 0,
+            completed: false
+        }
+    ]
+
+    constructor() {
+        this.todos.id = uuidv4()
+    }
+
+    index() {
+        return this.todos
+    }
+
+    create(todo) {
+        const { content, completed } = todo
+
+        if (content !== '' && typeof completed === 'boolean')
+            this.todos.push(todo)
+    }
+
+}
