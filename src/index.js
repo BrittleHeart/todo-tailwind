228205c5d2ac8c4f7938ddcdfc35f57b0e190649
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-26 18:45:23 +0100

    Added small javascript class to handle snowpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import { Todo } from '../todo/todo';
+
+const newTodo = new Todo();
+
+newTodo.content = "First Todo"
+newTodo.id = 1
+newTodo.completed = false
+
+console.log(newTodo);
---
+++ todo/todo.js
@@ -0,0 +1,6 @@
+export function Todo () {
+    this.content = ''
+    this.id = 1
+    this.completed = false
+}
+
